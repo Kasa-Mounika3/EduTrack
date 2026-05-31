f8c0d1d045da97b191be35cc87c10186296aa9d5
Kasa Mounika <24r11a05n2@gcet.edu.in>
2026-05-31 16:45:41 +0530

    Fix academic API route

--- Level-2/client/src/services/academicService.js
+++ Level-2/client/src/services/academicService.js
@@ -2,23 +2,23 @@ import apiClient from './apiClient.js';
 
 export const academicService = {
   list: async () => {
-    const res = await apiClient.get('/academics');
+    const res = await apiClient.get('/academic');
     return res.data.data;
   },
   createYear: async (payload) => {
-    const res = await apiClient.post('/academics/years', payload);
+    const res = await apiClient.post('/academic/years', payload);
     return res.data.data;
   },
   createDepartment: async (payload) => {
-    const res = await apiClient.post('/academics/departments', payload);
+    const res = await apiClient.post('/academic/departments', payload);
     return res.data.data;
   },
   createSection: async (payload) => {
-    const res = await apiClient.post('/academics/sections', payload);
+    const res = await apiClient.post('/academic/sections', payload);
     return res.data.data;
   },
   createSubject: async (payload) => {
-    const res = await apiClient.post('/academics/subjects', payload);
+    const res = await apiClient.post('/academic/subjects', payload);
     return res.data.data;
   }
 };
--- Level-3/client/src/services/academicService.js
+++ Level-3/client/src/services/academicService.js
@@ -2,23 +2,23 @@ import api from './api.js';
 
 export const academicService = {
   list: async () => {
-    const res = await api.get('/academics');
+    const res = await api.get('/academic');
     return res.data.data;
   },
   createYear: async (payload) => {
-    const res = await api.post('/academics/years', payload);
+    const res = await api.post('/academic/years', payload);
     return res.data.data;
   },
   createDepartment: async (payload) => {
-    const res = await api.post('/academics/departments', payload);
+    const res = await api.post('/academic/departments', payload);
     return res.data.data;
   },
   createSection: async (payload) => {
-    const res = await api.post('/academics/sections', payload);
+    const res = await api.post('/academic/sections', payload);
     return res.data.data;
   },
   createSubject: async (payload) => {
-    const res = await api.post('/academics/subjects', payload);
+    const res = await api.post('/academic/subjects', payload);
     return res.data.data;
   }
 };
